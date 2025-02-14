37e6d052ce91f914fb958d4f59d2cb4e6079167f
Artem Kuznetsov <AKuznetsov@1bit.com>
2025-02-14 14:40:23 +0300

    refactor: В скриптах преобразования переработан подход к поиску расширений.

--- tools/scripts/upd_ib.cmd
+++ tools/scripts/upd_ib.cmd
@@ -12,8 +12,6 @@ IF not defined V8_BRANCH set V8_BRANCH=local
 
 set RELATIVE_REPO_PATH=%~dp0..\..
 set RELATIVE_SRC_PATH=src
-set RELATIVE_SRC_CF_PATH=cf
-set RELATIVE_SRC_CFE_PATH=cfe
 
 FOR /F "usebackq tokens=1 delims=" %%i IN (`FORFILES /P "%RELATIVE_REPO_PATH%" /M "%RELATIVE_SRC_PATH%" /C "cmd /c echo @path"`) DO set SRC_PATH=%%i
 IF not defined SRC_PATH (
@@ -33,21 +31,10 @@ FOR /L %%i IN (1, 1, 10) DO (
 
 IF exist "%REPO_PATH%\.env" (
     FOR /F "usebackq tokens=*" %%a in ("%REPO_PATH%\.env") DO (
-        FOR /F "tokens=1,2 delims==" %%b IN ("%%a") DO (
+        FOR /F "tokens=1* delims==" %%b IN ("%%a") DO ( 
             set "%%b=%%c"
         )
     )
-)
-
-FOR /F "usebackq tokens=1 delims=" %%i IN (`FORFILES /P "%SRC_PATH%" /M "%RELATIVE_SRC_CF_PATH%" /C "cmd /c echo @path"`) DO (
-    set CONF_PATH=%%i
-    set CONF_PATH=!CONF_PATH:"=!
-    echo [INFO] Found main configuration folder "!CONF_PATH!"
-)
-FOR /F "usebackq tokens=1 delims=" %%i IN (`FORFILES /P "%SRC_PATH%" /M "%RELATIVE_SRC_CFE_PATH%" /C "cmd /c echo @path"`) DO (
-    set EXT_PATH=%%i
-    set EXT_PATH=!EXT_PATH:"=!
-    echo [INFO] Found extensions root folder "!EXT_PATH!"
 )
 
 set FILE_NAME=%~n0
@@ -59,25 +46,47 @@ IF not defined V8_IB_NAME (
 
 IF exist "%REPO_PATH%\%V8_IB_NAME%.env" (
     FOR /F "usebackq tokens=*" %%a in ("%REPO_PATH%\%V8_IB_NAME%.env") DO (
-        FOR /F "tokens=1,2 delims==" %%b IN ("%%a") DO (
+        FOR /F "tokens=1* delims==" %%b IN ("%%a") DO (
             set "%%b=%%c"
         )
     )
 )
 
+IF /i "%V8_SRC_TYPE%" equ "edt" (
+    set RELATIVE_CF_PATH=main
+) ELSE (
+    set RELATIVE_CF_PATH=cf
+    set RELATIVE_CFE_PATH=cfe
+)
+
+FOR /F "usebackq tokens=1 delims=" %%i IN (`FORFILES /P "%SRC_PATH%" /M "%RELATIVE_CF_PATH%" /C "cmd /c echo @path"`) DO (
+    set CONF_PATH=%%i
+    set CONF_PATH=!CONF_PATH:"=!
+    echo [INFO] Found main configuration folder "!CONF_PATH!"
+)
+IF defined RELATIVE_CFE_PATH (
+    FOR /F "usebackq tokens=1 delims=" %%i IN (`FORFILES /P "%SRC_PATH%" /M "%RELATIVE_CFE_PATH%" /C "cmd /c echo @path"`) DO (
+        set EXT_PATH=%%i
+        set EXT_PATH=!EXT_PATH:"=!
+        echo [INFO] Found extensions root folder "!EXT_PATH!"
+    )
+) ELSE (
+    set EXT_PATH=%SRC_PATH%
+)
+
 IF defined V8_IMPORT_TOOL set V8_CONVERT_TOOL=%V8_IMPORT_TOOL%
+IF not "%V8_CONVERT_TOOL%" equ "designer" IF not "%V8_CONVERT_TOOL%" equ "ibcmd" set V8_CONVERT_TOOL=designer
 
 set V8_CONNECTION_STRING="/S%V8_DB_SRV_ADDR%\%V8_IB_NAME%"
 IF /i "%V8_CONVERT_TOOL%" equ "designer" set V8_CONNECTION_STRING="/S%V8_SRV_ADDR%\%V8_IB_NAME%"
 
-IF not "%V8_CONVERT_TOOL%" equ "designer" IF not "%V8_CONVERT_TOOL%" equ "ibcmd" set V8_CONVERT_TOOL=designer
-IF not defined V8_TOOL set V8_TOOL="C:\Program Files\1cv8\%V8_VERSION%\bin\1cv8.exe"
+IF not defined V8_TOOL set V8_TOOL="%PROGRAMW6432%\1cv8\%V8_VERSION%\bin\1cv8.exe"
 IF "%V8_CONVERT_TOOL%" equ "designer" IF not exist %V8_TOOL% (
     echo [ERROR] Could not find 1C:Designer with path %V8_TOOL%
     set ERROR_CODE=1
     goto finally
 )
-IF not defined IBCMD_TOOL set IBCMD_TOOL="C:\Program Files\1cv8\%V8_VERSION%\bin\ibcmd.exe"
+IF not defined IBCMD_TOOL set IBCMD_TOOL="%PROGRAMW6432%\1cv8\%V8_VERSION%\bin\ibcmd.exe"
 IF "%V8_CONVERT_TOOL%" equ "ibcmd" IF not exist %IBCMD_TOOL% (
     echo [ERROR] Could not find ibcmd tool with path %IBCMD_TOOL%
     set ERROR_CODE=1
@@ -99,19 +108,53 @@ FOR /f "tokens=1 delims=" %%a in (' "!GIT_COMMAND!" ') do (
     set ACTUAL_COMMIT=%%a
     echo [INFO] Actual commit "!ACTUAL_COMMIT!"
 )
-IF not defined V8_EXTENSIONS (
-    FOR /F "usebackq tokens=1 delims=" %%i IN (`FORFILES /P "%EXT_PATH%" /C "cmd /c echo @path"`) DO (
-        set EXT_NAME=%%i
-        set EXT_NAME=!EXT_NAME:%EXT_PATH%\=!
-        set EXT_NAME=!EXT_NAME:"=!
-        echo [INFO] Found extension "!EXT_NAME!"
-        IF not defined V8_EXTENSIONS (
-            set V8_EXTENSIONS=!EXT_NAME!
-        ) ELSE (
-            set V8_EXTENSIONS=!V8_EXTENSIONS! !EXT_NAME!
+
+IF "%V8_EXTENSIONS%" equ "ib" (
+    set V8_EXTENSIONS=
+    IF "%V8_CONVERT_TOOL%" equ "designer" (
+        set EXT_LIST_FILE=%~dp0v8_ext_list.txt
+        %V8_TOOL% DESIGNER /IBConnectionString !V8_IB_CONNECTION! /N"%V8_IB_USER%" /P"%V8_IB_PWD%" /DisableStartupDialogs  /DisableStartupMessages /Out "!EXT_LIST_FILE!" /DumpDBCfgList -AllExtensions
+        FOR /F "tokens=* delims=" %%i IN (!EXT_LIST_FILE!) DO (
+            set EXT_NAME=%%i
+            set EXT_NAME=!EXT_NAME: =!
+            set EXT_NAME=!EXT_NAME:"=!
+            IF /i "!EXT_NAME!" equ "%%i" (
+                echo [INFO] Found extension in infobase: !EXT_NAME!
+                IF defined V8_EXTENSIONS (
+                    set V8_EXTENSIONS=!V8_EXTENSIONS! !EXT_NAME!
+                ) ELSE (
+                    set V8_EXTENSIONS=!EXT_NAME!
+                )
+            )
         )
+        del /f /s /q "!EXT_LIST_FILE!" > nul
+        goto process_ext
+    ) ELSE (
+        set "COMMAND_EXT_LIST=%IBCMD_TOOL% infobase config extension list --dbms=%V8_DB_SRV_DBMS% --db-server=%V8_DB_SRV_ADDR% --db-name="%V8_IB_NAME%" --db-user="%V8_DB_SRV_USR%" --db-pwd="%V8_DB_SRV_PWD%" --user="%V8_IB_USER%" --password="%V8_IB_PWD%""
+        FOR /f "tokens=1,2 delims==:" %%i IN (' "!COMMAND_EXT_LIST!" ') DO (
+            set PARAM_NAME=%%i
+            set PARAM_NAME=!PARAM_NAME: =!
+            IF "!PARAM_NAME!" equ "name" (
+                set EXT_NAME=%%j
+                set EXT_NAME=!EXT_NAME: =!
+                set EXT_NAME=!EXT_NAME:"=!
+                echo [INFO] Found extension in infobase: !EXT_NAME!
+                IF defined V8_EXTENSIONS (
+                    set V8_EXTENSIONS=!V8_EXTENSIONS! !EXT_NAME!
+                ) ELSE (
+                    set V8_EXTENSIONS=!EXT_NAME!
+                )
+            )
+        )
+        goto process_ext
     )
 )
+
+IF defined V8_EXTENSIONS (
+    FOR %%j IN (%V8_EXTENSIONS%) DO echo [INFO] Found extension in environment settings: %%j
+)
+
+:process_all
 
 set V8_CONNECTION_STRING=/S%V8_DB_SRV_ADDR%\%V8_IB_NAME%
 IF /i "%V8_CONVERT_TOOL%" equ "designer" set V8_CONNECTION_STRING=/S%V8_SRV_ADDR%\%V8_IB_NAME%
@@ -186,10 +229,10 @@ FOR %%j IN (%V8_EXTENSIONS%) DO (
     IF "!EXT_CHANGED!" equ "1" (
         echo.
         echo ======
-        echo Import extension "%%j"
+        echo Import extension "!EXT_NAME!"
         echo ======
-        call %REPO_PATH%\tools\1CFilesConverter\scripts\ext2ib.cmd "%EXT_PATH%\%%j" "%V8_CONNECTION_STRING%" "%%j"
-        IF ERRORLEVEL 0 echo %V8_IB_NAME%:%ACTUAL_COMMIT%> "%EXT_PATH%\%%j\SYNC_COMMIT"
+        call %REPO_PATH%\tools\1CFilesConverter\scripts\ext2ib.cmd "%EXT_PATH%\!EXT_NAME!" "%V8_CONNECTION_STRING%" "!EXT_NAME!"
+        IF ERRORLEVEL 0 echo %V8_IB_NAME%:%ACTUAL_COMMIT%> "%EXT_PATH%\!EXT_NAME!\SYNC_COMMIT"
         IF not defined EXT_UPDATE_DB (
             set EXT_UPDATE_DB=!EXT_NAME!
         ) ELSE (
